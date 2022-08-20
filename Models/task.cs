d4833f93f3296eba9bf24b2ca354eaeed7b2c2bd
sreechaithanyaB <sree.chaithanya@rootquotient.com>
2022-08-20 22:32:31 +0530

    Create task.cs

---
+++ Models/task.cs
@@ -0,0 +1,9 @@
+
+namespace todolist.Models{
+    public class task{
+     
+        public int taskid{get; set;}
+         public string title{get; set;}
+        public string description{get; set;}
+    }
+}
