a5e39509bd9f842d2d8e14c4d41a60c640e98267
sreechaithanyaB <sree.chaithanya@rootquotient.com>
2022-08-20 23:38:12 +0530

    Create usersignin.cs

---
+++ Models/usersignin.cs
@@ -0,0 +1,10 @@
+namespace todolist.Models{
+    public class user_signin{
+     
+        public int firstname{get; set;}
+         public string lastname{get; set;}
+        public string email{get; set;}
+        public string username{get; set;}
+        public string password{get; set;}
+    }
+}
