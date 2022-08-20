7b020ff02a56200a77a2286c7179f9715a1bda10
sreechaithanyaB <sree.chaithanya@rootquotient.com>
2022-08-20 22:18:50 +0530

    Update TodoController.cs

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -5,104 +5,116 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using todolist.Models;
+using Newtonsoft.Json;
 
 namespace todolist.Controllers{
 [ApiController]
 [Route("[controller]")]
-    public class TodoController : ControllerBase{
-        private readonly TodoContext _context;
-        public TodoController(TodoContext context){
-            _context=context;
-            if(_context.todotasks.Count()==0){
-                _context.todotasks.Add(new todotask{title = "Item1", description = "item1Description"});
-                
-
-            
-            
-                _context.todotasks.Add(new todotask{title = "Item2", description = "item1Description2"});
-                _context.SaveChanges();
-
-            }
-        }
+public class TodoController : ControllerBase{
+private readonly TodoContext _context;
+        
+public TodoController(TodoContext context){
+ _context=context;
+}
     //get all tasks
 
     [HttpGet]
-    public ActionResult<List<todotask>> GetAll(){
-        return _context.todotasks.ToList();
 
-    }
+    public string GetAll(){
+        List<todotask> list= _context.todotasks.ToList();
+        todotaskResponseRoots t_d_t_r_r = new todotaskResponseRoots();
+        t_d_t_r_r.Tasks=new List<todotaskResponse>();
+        foreach (todotask t_d_t in list){
+        todotaskResponse t_d_t_r = new todotaskResponse(t_d_t);
+   
+                  
+        t_d_t_r_r.Tasks.Add(t_d_t_r);
+        }
+   return JsonConvert.SerializeObject(t_d_t_r_r);
+}
 
     //Get By id- a particular task
 
     [HttpGet("{id}",Name ="GetTodo")]
-    public ActionResult<todotask> GetById(int id){
-        var item = _context.todotasks.Find(id);
-        if(item==null){
-            return NotFound();
+    public string GetById(int id){
+        todotask t_d_t = _context.todotasks.Find(id);
+        if(t_d_t==null){
+        string a="Not found";
+        return a;
         }
-        return item;
-
-
-    }
+        todotaskResponse t_d_t_r = new todotaskResponse(t_d_t);
+        todotaskResponseRoot t_d_t_r_r = new todotaskResponseRoot();
+        t_d_t_r_r.Task=t_d_t_r;
+    return JsonConvert.SerializeObject(t_d_t_r_r);    }
 
     //create or add an task to the db
     [HttpPost]
-    public ActionResult<todotask> Create([FromBody] todotask item)
+
+    public string Create([FromBody] todotaskrequestRoot t_r_r_t)
     { 
-    if (item == null)
+    if (t_r_r_t == null)
     {
-        return BadRequest();
+        string a="Not found";
+        return a;
     }
-
-    _context.todotasks.Add(item);
-    _context.SaveChanges();
-
-    return Ok();
+        todotask t_d_t = todoTaskSerializer.createToDoTask(t_r_r_t);
+        _context.todotasks.Add(t_d_t);
+        _context.SaveChanges();
+    
+        todotaskResponse t_d_t_r = new todotaskResponse(t_d_t);
+        todotaskResponseRoot t_d_t_r_r = new todotaskResponseRoot();
+        t_d_t_r_r.Task=t_d_t_r;
+        string b= JsonConvert.SerializeObject(t_d_t_r_r);
+    return b;
     }
 
 
     //update: edit an partticular entry of the existing db
 
     [HttpPut("{id}")]
-    public ActionResult<todotask> Put(int id, [FromBody] todotask item)
+    public string Put(int id, [FromBody] todotaskrequestRoot t_r)
     {
-         if (item == null)
-        {
-            Console.WriteLine("this is the error");
-            return BadRequest();
-        }
-
-        var todotask = _context.todotasks.Find(id);
-        if (todotask == null)
-        {
-            return NotFound();
-        }
-
-        
-        todotask.title = item.title;
-        todotask.description= item.description;
-        
-
-        _context.todotasks.Update(todotask);
-        _context.SaveChanges();
-        return Ok();
+    if (t_r == null)
+    {
+        string a="Bad request";
+        return a;
+    }
+    todotask t_d_t = todoTaskSerializer.createToDoTask(t_r);
+    
+    todotask tt = _context.todotasks.Find(id);
+    if (tt == null)
+    {
+        string b="Not found";
+        return b;
+    }
+    tt.title = t_d_t.title;
+    tt.description= t_d_t.description;
+    _context.todotasks.Update(tt);
+    _context.SaveChanges();
+    todotaskResponse t_d_t_r = new todotaskResponse(tt);
+        todotaskResponseRoot t_d_t_r_r = new todotaskResponseRoot();
+        t_d_t_r_r.Task=t_d_t_r;
+    return JsonConvert.SerializeObject(t_d_t_r_r);
     }
 
 //Deletion of task by particular ID
 
 [HttpDelete("{id}")]
-    public ActionResult<todotask>  Delete(int id)
+    public string  Delete(int id)
     {
-        var todotask = _context.todotasks.Find(id);
-        if (todotask == null)
+        todotask t_d_t = _context.todotasks.Find(id);
+        if (t_d_t == null)
         {
-            return NotFound();
+            return "NotFound()";
         }
 
-        _context.todotasks.Remove(todotask);
+        _context.todotasks.Remove(t_d_t);
         _context.SaveChanges();
-        return Ok();
-    }
+        todotaskResponse t_d_t_r = new todotaskResponse(t_d_t);
+        todotaskResponseRoot t_d_t_r_r = new todotaskResponseRoot();
+        t_d_t_r_r.Task=t_d_t_r;
+    return JsonConvert.SerializeObject(t_d_t_r_r);
+}
 
 
 
