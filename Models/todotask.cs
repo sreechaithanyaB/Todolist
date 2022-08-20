5fbbf2d6b9a10dc11503905283dcf0482be2a768
sreechaithanyaB <sree.chaithanya@rootquotient.com>
2022-08-20 23:22:37 +0530

    Update todotask.cs

--- Models/todotask.cs
+++ Models/todotask.cs
@@ -1,8 +1,11 @@
+
+using System.Text.Json.Serialization;
+
 namespace todolist.Models{
+
     public class todotask{
         public int id{get; set;}
-        public string title{get; set;}
-        public string description{get; set;}
+        public string title {get; set;}
+       public string description { get; set; }
     }
 }
-
